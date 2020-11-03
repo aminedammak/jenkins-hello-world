2be76789869404dfab4977eff5be94616cbdd3ca
adammak <aminedammak86@gmail.com>
2020-11-03 16:37:11 +0100

    Message 14

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void  main(String[] args) {
         for (int i = 0 ; i <=10 ; i++) {
-            System.out.println("Hello from jenkins13" + i);
+            System.out.println("Hello from jenkins14" + i);
         }
     }
 }
